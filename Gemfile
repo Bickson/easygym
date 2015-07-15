cac71f3175a1965968d75e03bf63623677be6fa5
darioflorez <florez.wilmar@gmail.com>
2015-07-15 15:10:57 +0200

    capybara and rspec added to the project

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '3.2.18'
 gem 'sqlite3'
 
 gem 'react-rails', '~> 1.0'
+gem 'rspec-rails'
+gem 'capybara'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/assets/javascripts/components/exercise.js.jsx
+++ app/assets/javascripts/components/exercise.js.jsx
@@ -1,10 +1,26 @@
 var Exercise = React.createClass({
 
-	render: function(){
+	/*render: function(){
+		//var createItem = (exercise) => <li>{exercise.name}</li>;
 		return(
-			<div>
-				<p> HEJ HEJ </p>
-			</div>
-			);
-	}
+
+			<p> hej </p>
+			//<ul>{this.props.exercise.map(createItem)}</ul>
+			
+		);
+	}*/
+	 render () {
+	 	return <h2> Hi {this.props.exercise}!</h2>;
+	 }
 });
+
+/** ES6 Javascript 6 */
+/*class Exercise extends React.Component {
+	render(){
+		return (<h2> Hi {this.props.exercise}!</h2>);
+	}
+}
+
+Exercise.propTypes = {
+		exercise: React.PropTypes.string
+};*/
